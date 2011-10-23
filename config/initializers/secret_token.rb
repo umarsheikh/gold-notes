d8f464d25f9c3c90855ab7e6d312a6818ee8a0a0
Umar Sheikh <umar.m.sheikh@gmail.com>
2011-10-23 18:16:20 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoldNotes::Application.config.secret_token = '5c91fab9fe6168fc7e0cf1707a101ffb6c9366e03b41eb25eeb194d4a2d0efcd74d9ee983705e2391a4682225f060130c63df28f1437a90d287556220f9a12a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GoldNotes::Application.config.session_store :cookie_store, :key => '_gold-notes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GoldNotes::Application.config.session_store :active_record_store
