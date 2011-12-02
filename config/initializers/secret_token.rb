9b392412c2e60304a0bc62cdfd7fe4d7d7653c49
Umar Sheikh <umar.m.sheikh@gmail.com>
2011-12-02 13:17:08 +0500

    proper commit of a rails 3 application

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GoldNotes::Application.config.secret_token = '5c91fab9fe6168fc7e0cf1707a101ffb6c9366e03b41eb25eeb194d4a2d0efcd74d9ee983705e2391a4682225f060130c63df28f1437a90d287556220f9a12a5'
+GoldNotes::Application.config.secret_token = '50274546f20aa451ac195f6c359bc9245dd8a6034d5cc4ce380f199e0dcef2fad78df69b44067f770aea60587fc94a44626bbb59f84a37c8f50a36a404a233cb'
