fba8cc2b5eb2ef3a77cbd7bcf16e8e9e5963a46f
Umar Sheikh <umar.m.sheikh@gmail.com>
2012-03-08 20:49:48 +0500

    rails 3.2 app now works, rails new gold-notes with lots of updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GoldNotes::Application.config.secret_token = '50274546f20aa451ac195f6c359bc9245dd8a6034d5cc4ce380f199e0dcef2fad78df69b44067f770aea60587fc94a44626bbb59f84a37c8f50a36a404a233cb'
+GoldNotes::Application.config.secret_token = '7bd1b49dbf0d2bad5636ce184d68d463e69be3e478758c6fa319157eeebccac0a1e6a7a42a511a5628f071a5283578711be8174ea2dcf87c14363d680560dfbe'
