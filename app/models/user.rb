2c0f49ca64c9bfe127cd84d17d8e2576592d8a9c
Umar Sheikh <umar.m.sheikh@gmail.com>
2012-03-22 18:02:08 +0500

    an initial version of threaded save to get the hang of it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,34 @@
 require 'inline'
 class User < ActiveRecord::Base
+  validates_uniqueness_of :name
   inline do |builder|
+    builder.include "<pthread.h>"
+
     builder.c "
       long factorial(int max) {
         int i=max, result=1;
         while (i >= 2) { result *= i--; }
         return result;
       }"
-   end
+
+  end
+
+  def self.example_threader(list_of_names = [['hello','mellow'],['harry','larry'],['umar','sheikh']])
+
+    threads = []
+    puts "usr count #{User.count}"
+    list_of_names.each do |name_sub_array|
+      threads << Thread.new(name_sub_array) do |names|
+        puts "creating #{names.length} records for #{names}"
+        names.each do |n|
+          User.find_or_create_by_name(n)
+        end
+      end
+    end
+
+    threads.each do |t|
+      t.join
+    end
+    puts "user count #{User.count}"
+  end
 end
