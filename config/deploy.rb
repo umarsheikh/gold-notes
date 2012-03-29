e5f433b68082b624ca160446e251595f86017090
Umar Sheikh <umar.m.sheikh@gmail.com>
2012-03-29 21:27:22 +0500

    making two deploys work from same project without having two git repos

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require 'rvm/capistrano'
 set :rvm_ruby_string, 'ruby-1.9.3-p0@gold'
 set :rvm_type, :user
 require 'bundler/capistrano'
-
+default_run_options[:pty] = true # if u run a command on server which requesta pwd, then this request gets forwarded to you
 set :application, "gold-notes"
 set :repository,  "."
 set :scm, :none
@@ -45,3 +45,27 @@ namespace :deploy do
   end
 end
 
+# the following is an example of how to have two deploys from the same project, one from Rails.root, 
+# the one on top, and the other from Rails.root/utils/import/jenkins, as this requires the bundle 
+# install to run, you will need to create an empty Gemfile in the utils/import/jenkins directory, and 
+# as the symlinks wont be there in the deploy:symlink_config_files task, you will need to wrap those 
+# creations is a block like: if release_path.match(/jenkins/) else end
+
+namespace :deploy do
+  after "deploy:jenkins", "deploy"
+  after "deploy:jenkins_setup", "deploy:setup"
+  desc "setup jenkins deployment directories before doing a jenkins deploy for first time"
+  task :jenkins_setup, :roles => :app do
+    configure_jenkins
+  end
+  desc "deploy the jenkins folder"
+  task :jenkins, :roles => :app do
+    configure_jenkins()
+  end
+  task :configure_jenkins, :roles => :app do
+    set :application, "jenkins"
+    set :repository,  "./utils/import/jenkins"
+    set :deploy_to, "~/apps/jenkins"
+    set :bundle_flags, "--quiet"
+  end
+end
